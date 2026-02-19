d2bcf2d9e4afd63cf268cfa19a72786611d04dc8
Rasmus <rashansen69@gmail.com>
2026-02-19 12:19:20 +0100

    Hej

--- exploratory/rasmus/kigpådata.R
+++ exploratory/rasmus/kigpådata.R
@@ -18,3 +18,12 @@ View(test_data)
 guess <- data.frame(id = test_data$id, target = mean_guess)
 View(guess)
 
+write.csv(guess, "./inst/eksempel_model/resultater/gennemsnit.csv", row.names = FALSE)
+
+
+
+
+
+View(training_data)
+
+
