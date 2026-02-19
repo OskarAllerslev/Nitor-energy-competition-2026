c4d4bae00fbb80a4aaabb1725bba183485472e75
Rasmus <rashansen69@gmail.com>
2026-02-19 11:25:47 +0100

    Korrekt folder structure og gennemsnitsmodel

---
+++ exploratory/rasmus/kigpådata.R
@@ -0,0 +1,20 @@
+
+training_data <- readr::read_csv("data/train.csv")
+View(training_data)
+
+
+
+mean_guess <- mean(training_data$target)
+
+
+
+
+
+
+
+test_data <- readr::read_csv("data/test_for_participants.csv")
+View(test_data)
+
+guess <- data.frame(id = test_data$id, target = mean_guess)
+View(guess)
+
