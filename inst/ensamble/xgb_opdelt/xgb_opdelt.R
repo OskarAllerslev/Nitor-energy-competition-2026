173066700191dcd644875d7b8770126c4ef1c3af
Rasmus <rashansen69@gmail.com>
2026-02-22 19:12:42 +0100

    Preparing for final submission

--- inst/ensamble/xgb_opdelt/xgb_opdelt.R
+++ inst/ensamble/xgb_opdelt/xgb_opdelt.R
@@ -19,13 +19,16 @@ tune_model <- function(MARKED = "A", sub_model_name, training_split, variance_da
     sub_model_name = sub_model_name,
     training_split = training_split,
     wrks =wrks,
-    step = 50,
+    step = 20,
     variance_damping = variance_damping
   )
 }
 
-sub_model_name <- "shorthorizonandtweedieforA"
-#xgb_best_params_A <- tune_model("A", sub_model_name, training_split)
+sub_model_name <- "final model"
+
+# This code was used for tuning
+
+# xgb_best_params_A <- tune_model("A", sub_model_name, training_split)
 # xgb_best_params_B <- tune_model("B", sub_model_name, training_split)
 # xgb_best_params_C <- tune_model("C", sub_model_name, training_split)
 # xgb_best_params_D <- tune_model("D", sub_model_name, training_split)
@@ -33,7 +36,8 @@ sub_model_name <- "shorthorizonandtweedieforA"
 # xgb_best_params_F <- tune_model("F", sub_model_name, training_split)
 
 
-#Final parameters after tuning
+# Final parameters after using the tuning code above
+
 xgb_best_params_A <- readRDS("./inst/xgb_Market A/model/best_params_onebigfoldfirstperiodremoveddoneproperlyxgb_Market A22-02-2026 16-48-18.rds")
 xgb_best_params_B <- readRDS("./inst/xgb_Market B/model/best_params_onebigfoldfirstperiodremoveddoneproperlyxgb_Market B22-02-2026 16-49-20.rds")
 xgb_best_params_C <- readRDS("./inst/xgb_Market C/model/best_params_onebigfoldfirstperiodremoveddoneproperlyxgb_Market C22-02-2026 16-50-21.rds")
@@ -41,9 +45,9 @@ xgb_best_params_D <- readRDS("./inst/xgb_Market D/model/best_params_onebigfoldfi
 xgb_best_params_E <- readRDS("./inst/xgb_Market E/model/best_params_onebigfoldfirstperiodremoveddoneproperlyxgb_Market E22-02-2026 16-52-24.rds")
 xgb_best_params_F <- readRDS("./inst/xgb_Market F/model/best_params_onebigfoldfirstperiodremoveddoneproperlyxgb_Market F22-02-2026 16-53-21.rds")
 
-#xgb_best_params_A <- readRDS()
-
 # så skal de fittes ----
+
+#helper functions for fitting
 
 get_fit <- function(best_params, MARKED = "A", training_split, variance_damping = T) {
   train_df <- get_train_df(glue::glue("Market {MARKED}"), training_split = training_split, variance_damping = variance_damping)
@@ -70,15 +74,14 @@ predict_on_market_model <- function(data_for_prediction, MARKED = "A", market_fi
 }
 
 ## Fit on training split to get diagnostics
+train_fit_a <- get_fit(xgb_best_params_A, "A", training_split)
 train_fit_b <- get_fit(xgb_best_params_B, "B", training_split)
 train_fit_c <- get_fit(xgb_best_params_C, "C", training_split)
 train_fit_d <- get_fit(xgb_best_params_D, "D", training_split)
 train_fit_e <- get_fit(xgb_best_params_E, "E", training_split)
 train_fit_f <- get_fit(xgb_best_params_F, "F", training_split)
 
-#Seperate model for market A
-predictions_A <- market_A_tweedie_model(training_split)  |> dplyr::filter(market == "Market A")
-
+predictions_A <- predict_on_market_model(data_for_prediction, "A", train_fit_a, training_split)
 predictions_B <- predict_on_market_model(data_for_prediction, "B", train_fit_b, training_split)
 predictions_C <- predict_on_market_model(data_for_prediction, "C", train_fit_c, training_split)
 predictions_D <- predict_on_market_model(data_for_prediction, "D", train_fit_d, training_split)
@@ -86,40 +89,36 @@ predictions_E <- predict_on_market_model(data_for_prediction, "E", train_fit_e, 
 predictions_F <- predict_on_market_model(data_for_prediction, "F", train_fit_f, training_split)
 
 data.frame(market= c("A","B","C","D","E","F"),
-      RMSE=rbind(predictions_A |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
-      predictions_B |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
-      predictions_C |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
-      predictions_D |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
-      predictions_E |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
-      predictions_F |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y)))
+           RMSE=rbind(predictions_A |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
+                      predictions_B |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
+                      predictions_C |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
+                      predictions_D |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
+                      predictions_E |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y),
+                      predictions_F |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y)))
 
 
-combined_predictionsNotA <- rbind(predictions_B, predictions_C, predictions_D, predictions_E, predictions_F)
-rbind(extract_fit_subset(combined_predictionsNotA, testing_split) |> dplyr::select(id, target.x, target.y),
-                          extract_fit_subset(predictions_A, testing_split) |> dplyr::select(id, target.x, target.y))|> yardstick::rmse(target.x, target.y)
+combined_predictions <- rbind(predictions_A, predictions_B, predictions_C, predictions_D, predictions_E, predictions_F) |> dplyr::arrange(id)
+combined_predictions |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y)
 
 ## Fit on full dataset for final submission
 
 # Use training_data instead of training_split to get all data including testing_split
+full_fit_a <- get_fit(xgb_best_params_A, "A", training_data)
 full_fit_b <- get_fit(xgb_best_params_B, "B", training_data)
 full_fit_c <- get_fit(xgb_best_params_C, "C", training_data)
 full_fit_d <- get_fit(xgb_best_params_D, "D", training_data)
 full_fit_e <- get_fit(xgb_best_params_E, "E", training_data)
 full_fit_f <- get_fit(xgb_best_params_F, "F", training_data)
 
-predictions_A_full <- market_A_tweedie_model(training_data)  |> dplyr::filter(market == "Market A")  |> dplyr::mutate(target = tidyr::replace_na(target, median(training_data$target, na.rm = T)))
+predictions_A_full <- predict_on_market_model(data_for_prediction, "A", full_fit_a, training_data)
 predictions_B_full <- predict_on_market_model(data_for_prediction, "B", full_fit_b, training_data)
 predictions_C_full <- predict_on_market_model(data_for_prediction, "C", full_fit_c, training_data)
 predictions_D_full <- predict_on_market_model(data_for_prediction, "D", full_fit_d, training_data)
 predictions_E_full <- predict_on_market_model(data_for_prediction, "E", full_fit_e, training_data)
 predictions_F_full <- predict_on_market_model(data_for_prediction, "F", full_fit_f, training_data)
 
-combined_predictions_full_notA <- rbind(predictions_B_full, predictions_C_full, predictions_D_full, predictions_E_full, predictions_F_full)
-combined_predictions_full <- rbind(extract_data_for_prediction(combined_predictions_full_notA), extract_data_for_prediction(predictions_A_full)) |> dplyr::arrange(id)
-
-
-
-combined_predictions_full |> save_results("xgb_opdelt", sub_model_name)
+combined_predictions_full <- rbind(predictions_A_full, predictions_B_full, predictions_C_full, predictions_D_full, predictions_E_full, predictions_F_full) |> dplyr::arrange(id)
+combined_predictions_full |> extract_data_for_prediction() |> save_results("xgb_opdelt", sub_model_name)
 
 
 
