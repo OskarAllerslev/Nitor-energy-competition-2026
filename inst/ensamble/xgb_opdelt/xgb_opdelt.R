f04e3846eda1ff8cb5844d35004cab2a9ca3796c
Rasmus <rashansen69@gmail.com>
2026-02-22 13:42:42 +0100

    First attempt at split model

--- inst/ensamble/xgb_opdelt/xgb_opdelt.R
+++ inst/ensamble/xgb_opdelt/xgb_opdelt.R
@@ -38,15 +38,6 @@ get_fit <- function(best_params, MARKED = "A", training_split) {
   finalize_workflow_helper(best_params, workflow_helper(train_df), train_df)
 }
 
-
-
-train_fit_a <- get_fit(xgb_best_params_A, "A", training_split)
-train_fit_b <- get_fit(xgb_best_params_B, "B", training_split)
-train_fit_c <- get_fit(xgb_best_params_C, "C", training_split)
-train_fit_d <- get_fit(xgb_best_params_D, "D", training_split)
-train_fit_e <- get_fit(xgb_best_params_E, "E", training_split)
-train_fit_f <- get_fit(xgb_best_params_F, "F", training_split)
-
 data_for_prediction <- prepare_data_for_prediction(function(x) {final_data_transformation(data.frame(target = 0))(x,F)})
 
 predict_on_market_model <- function(data_for_prediction, MARKED = "A", market_fit, training_split) {
@@ -64,6 +55,17 @@ predict_on_market_model <- function(data_for_prediction, MARKED = "A", market_fi
   return(data_to_predict_on)
 }
 
+## Fit on training split to get diagnostics
+
+train_fit_a <- get_fit(xgb_best_params_A, "A", training_split)
+train_fit_b <- get_fit(xgb_best_params_B, "B", training_split)
+train_fit_c <- get_fit(xgb_best_params_C, "C", training_split)
+train_fit_d <- get_fit(xgb_best_params_D, "D", training_split)
+train_fit_e <- get_fit(xgb_best_params_E, "E", training_split)
+train_fit_f <- get_fit(xgb_best_params_F, "F", training_split)
+
+
+
 predictions_A <- predict_on_market_model(data_for_prediction, "A", train_fit_a, training_split)
 predictions_B <- predict_on_market_model(data_for_prediction, "B", train_fit_b, training_split)
 predictions_C <- predict_on_market_model(data_for_prediction, "C", train_fit_c, training_split)
@@ -80,9 +82,27 @@ data.frame(market= c("A","B","C","D","E","F"),
       predictions_F |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y)))
 
 
-combined_predictions <- rbind(predictions_A, predictions_B, predictions_C, predictions_D, predictions_E, predictions_F)
+combined_predictions <- rbind(predictions_A, predictions_B, predictions_C, predictions_D, predictions_E, predictions_F) |> dplyr::arrange(id)
 combined_predictions |> extract_fit_subset(testing_split) |> yardstick::rmse(target.x, target.y)
 
 
 
+## Fit on full dataset for final submission
 
+# Use training_data instead of training_split to get all data including testing_split
+full_fit_a <- get_fit(xgb_best_params_A, "A", training_data)
+full_fit_b <- get_fit(xgb_best_params_B, "B", training_data)
+full_fit_c <- get_fit(xgb_best_params_C, "C", training_data)
+full_fit_d <- get_fit(xgb_best_params_D, "D", training_data)
+full_fit_e <- get_fit(xgb_best_params_E, "E", training_data)
+full_fit_f <- get_fit(xgb_best_params_F, "F", training_data)
+
+predictions_A_full <- predict_on_market_model(data_for_prediction, "A", full_fit_a, training_data)
+predictions_B_full <- predict_on_market_model(data_for_prediction, "B", full_fit_b, training_data)
+predictions_C_full <- predict_on_market_model(data_for_prediction, "C", full_fit_c, training_data)
+predictions_D_full <- predict_on_market_model(data_for_prediction, "D", full_fit_d, training_data)
+predictions_E_full <- predict_on_market_model(data_for_prediction, "E", full_fit_e, training_data)
+predictions_F_full <- predict_on_market_model(data_for_prediction, "F", full_fit_f, training_data)
+
+combined_predictions_full <- rbind(predictions_A_full, predictions_B_full, predictions_C_full, predictions_D_full, predictions_E_full, predictions_F_full) |> dplyr::arrange(id)
+combined_predictions_full |> extract_data_for_prediction() |> save_results("xgb_opdelt", "FirstAttemptAtSplitModel")
